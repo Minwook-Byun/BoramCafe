d934e928eee1218fc92b8424ce37ec52af59095a
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-21 14:00:51 +0900

    💄 Feat: 추가 글 발행 기능(변수이용)

--- boramchancafe/src/App.js
+++ boramchancafe/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 import ALTER_IMG from './img/alter_img.PNG';
+import IMG_1 from './img/1.jpg';
+import IMG_2 from './img/2.jpg';
+import IMG_3 from './img/3.jpg';
 
 function App() {
   let [글제목, 글제목변경] = useState([
@@ -11,30 +14,55 @@ function App() {
   let [like, likeOnClick] = useState(0);
   let [modalSwitch, modalSwitchToggle] = useState(true);
   let [ClickedModalTitle, ChangeModalTitle] = useState(0);
+  let [displayImgs, changeDisplayImgs] = useState([IMG_1, IMG_2, IMG_3]);
+
+  let [userInput, changeUserInput] = useState(' ');
+
   return (
     <div className="App">
       <div className="global-nav">
         <h1>보람찬 카페</h1>
       </div>
+
       {글제목.map((title, i) => {
         return (
-          <ul className="post-main-lists">
-            <li>
-              <h2
-                onClick={() => {
-                  ChangeModalTitle(i);
-                }}
-              >
-                {title}
-              </h2>
-              {/* index가 들어가는 것이 아니라 elements가 각각 title에 들어가므로 
+          <div className="post-main-lists" key={i}>
+            <ul>
+              <li>
+                <h2
+                  onClick={() => {
+                    ChangeModalTitle(i);
+                  }}
+                >
+                  {title}
+                </h2>
+                {/* index가 들어가는 것이 아니라 elements가 각각 title에 들어가므로 
               글제목[title]과 같은 씽크빅하면 안 된다 */}
-              <span>👍</span>
-            </li>
-            <img alt="대체 텍스트, 나뭇잎 먹는 쿼카" src={ALTER_IMG} />
-          </ul>
+                <span>👍</span>
+              </li>
+              <img alt="대체 텍스트, 나뭇잎 먹는 쿼카" src={displayImgs[i]} />
+            </ul>
+          </div>
         );
       })}
+
+      <div className="publish-input">
+        <input
+          onChange={(e) => {
+            changeUserInput(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            let CopiedArray = [...글제목];
+            CopiedArray.unshift(userInput);
+            글제목변경(CopiedArray);
+          }}
+        >
+          저장
+        </button>
+      </div>
+
       <button
         onClick={() => {
           modalSwitchToggle(!modalSwitch);
@@ -43,7 +71,11 @@ function App() {
         더보기🙋
       </button>
       {modalSwitch === true ? (
-        <Modal 글제목={글제목} ClickedModalTitle={ClickedModalTitle} />
+        <Modal
+          글제목={글제목}
+          ClickedModalTitle={ClickedModalTitle}
+          displayImgs={displayImgs}
+        />
       ) : null}
     </div>
   );
@@ -53,7 +85,7 @@ function Modal(props) {
   return (
     <div className="modal">
       <h1>{props.글제목[props.ClickedModalTitle]}</h1>
-      <h2>내용</h2>
+      <h2> {props.displayImgs[1]} </h2>
       <span>날짜</span>
     </div>
   );
