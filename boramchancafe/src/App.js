3bcccfe17e16ef7b958545095fdf236baab8ca08
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-21 12:32:43 +0900

    Feat: 추가 모달에 title 클릭에 따라서 제목이 변하도록

--- boramchancafe/src/App.js
+++ boramchancafe/src/App.js
@@ -10,40 +10,49 @@ function App() {
   ]);
   let [like, likeOnClick] = useState(0);
   let [modalSwitch, modalSwitchToggle] = useState(true);
+  let [ClickedModalTitle, ChangeModalTitle] = useState(0);
   return (
     <div className="App">
       <div className="global-nav">
         <h1>보람찬 카페</h1>
       </div>
-      {글제목.map((title) => {
+      {글제목.map((title, i) => {
         return (
           <ul className="post-main-lists">
             <li>
-              {title}
+              <h2
+                onClick={() => {
+                  ChangeModalTitle(i);
+                }}
+              >
+                {title}
+              </h2>
               {/* index가 들어가는 것이 아니라 elements가 각각 title에 들어가므로 
               글제목[title]과 같은 씽크빅하면 안 된다 */}
               <span>👍</span>
             </li>
             <img alt="대체 텍스트, 나뭇잎 먹는 쿼카" src={ALTER_IMG} />
-            <button
-              onClick={() => {
-                modalSwitchToggle(modalSwitch === false);
-              }}
-            >
-              더보기🙋
-            </button>
-            {modalSwitch === true ? <Modal /> : null}
           </ul>
         );
-      })}{' '}
+      })}
+      <button
+        onClick={() => {
+          modalSwitchToggle(!modalSwitch);
+        }}
+      >
+        더보기🙋
+      </button>
+      {modalSwitch === true ? (
+        <Modal 글제목={글제목} ClickedModalTitle={ClickedModalTitle} />
+      ) : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div>
-      <h1>이름</h1>
+    <div className="modal">
+      <h1>{props.글제목[props.ClickedModalTitle]}</h1>
       <h2>내용</h2>
       <span>날짜</span>
     </div>
