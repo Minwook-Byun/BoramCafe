d7089d71b243490c309421babc49826c5ad3ecd4
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-22 12:09:11 +0900

    ✨ Feat: 추가 더보기 기능

--- boramchancafe/src/posts.js
+++ boramchancafe/src/posts.js
@@ -3,6 +3,7 @@ import ProductData from './data';
 
 const Post = () => {
   const [제품리스트, 제품명변경] = useState(ProductData);
+  const [moreSwitch, toggleMoreSwitch] = useState(false);
 
   return 제품리스트.map((item, i) => {
     return (
@@ -13,9 +14,23 @@ const Post = () => {
         </h2>
         <h3>{제품리스트[i].content}</h3>
         <span>{제품리스트[i].price}</span>
+
+        <button
+          className="contents-more"
+          onClick={() => {
+            toggleMoreSwitch(!moreSwitch);
+          }}
+        >
+          더보기
+        </button>
+        {moreSwitch === true ? <Modal /> : null}
       </div>
     );
   });
 };
+
+function Modal() {
+  return <div>ㅎㅇㅎㅇ</div>;
+}
 
 export default Post;
